b7ae9c5b86ecfed9ad2aae4fc39ce6a6b015f66e
Scott Kruger <scott@chojin.org>
2014-02-15 22:41:12 -0600

    Added runtime params

--- runserver.py
+++ runserver.py
@@ -1,5 +1,9 @@
 from party import app, initialize
+import party.config as config
 
 if __name__ == '__main__':
     initialize()
-    app.run(debug=True)
+    if config.FLASK_RUN_EXTERNALLY:
+      app.run(host='0.0.0.0', debug=config.FLASK_DEBUG)
+    else:
+      app.run(debug=config.FLASK_DEBUG)
