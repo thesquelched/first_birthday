1ffa8275424a8332c6f642e6588bda870acabe75
Scott Kruger <scott@chojin.org>
2014-02-16 17:52:33 -0600

    Added port option

--- runserver.py
+++ runserver.py
@@ -7,13 +7,14 @@ if __name__ == '__main__':
     initialize()
 
     params = dict(
-        debug=config.FLASK_DEBUG
+        debug=config.FLASK_DEBUG,
+        port=config.FLASK_PORT,
     )
 
     if config.SSL_ENABLED:
         context = SSL.Context(SSL.SSLv23_METHOD)
+        context.use_certificate_chain_file(config.SSL_CERT)
         context.use_privatekey_file(config.SSL_PRIVATE_KEY)
-        context.use_certificate_file(config.SSL_CERT)
 
         params['ssl_context'] = context
 
